3f3e821efd8b0bc06cc1dd7e4b52f42ab6bb4377
Gelin Luo <greenlaw110@gmail.com>
2015-05-18 08:28:00 +1000

    hello world now running!

--- fullstack-app/helloworld/src/main/java/org/osgl/ochestra/fsa/helloworld/HelloWorldApp.java
+++ fullstack-app/helloworld/src/main/java/org/osgl/ochestra/fsa/helloworld/HelloWorldApp.java
@@ -3,15 +3,23 @@ package org.osgl.ochestra.fsa.helloworld;
 import org.osgl.mvc.annotation.GetAction;
 import org.osgl.oms.boot.app.RunApp;
 
-import java.net.URL;
+import static org.osgl.oms.controller.Controller.Util.text;
 
 /**
- * The simple helloworld app
+ * The simple hello world app.
+ * <p>Run this app, try to update some of the code, then
+ * press F5 in the browser to watch the immediate change
+ * in the browser!</p>
  */
 public class HelloWorldApp {
     @GetAction("/hello")
     public String sayHello() {
-        return "hello world";
+        return "Hello Ochestra!";
+    }
+
+    @GetAction("/bye")
+    public void byePlay() {
+        text("bye Play!");
     }
 
     public static void main(String[] args) throws Exception {
