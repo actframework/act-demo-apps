f4e5233b180e4e1adcab59dbd01f267d6067b9d9
Gelin Luo <greenlaw110@gmail.com>
2015-11-29 23:01:33 +1100

    auto_obj project code refactory

--- fullstack-app/auto-object/src/main/java/act/fsa/auto_obj/AutoObjectApp.java
+++ fullstack-app/auto-object/src/main/java/act/fsa/auto_obj/AutoObjectApp.java
@@ -4,7 +4,6 @@ import act.boot.app.RunApp;
 import act.controller.Controller;
 import org.osgl.$;
 import org.osgl.mvc.annotation.GetAction;
-import org.osgl.mvc.annotation.PostAction;
 import org.osgl.mvc.result.Result;
 
 public class AutoObjectApp extends Controller.Util {
@@ -17,24 +16,14 @@ public class AutoObjectApp extends Controller.Util {
         return render();
     }
 
-    @GetAction("/foo1")
-    public Foo getFoo1() {
-        return foo1;
+    @GetAction("/foo/{id}")
+    public Foo getFoo(int id) {
+        return dispatch(id, $.F.<Foo>identity());
     }
 
-    @GetAction("/foo2")
-    public Foo getFoo2() {
-        return foo2;
-    }
-
-    @GetAction("/foo1/hc")
-    public int hashCode1() {
-        return foo1.hashCode();
-    }
-
-    @GetAction("/foo2/hc")
-    public int hashCode2() {
-        return foo2.hashCode();
+    @GetAction("/foo/{id}/hc")
+    public int hashCode(int id) {
+        return dispatch(id, $.F.<Foo>hc());
     }
 
     @GetAction("/eq")
@@ -44,6 +33,18 @@ public class AutoObjectApp extends Controller.Util {
 
     public static void main(String[] args) throws Exception {
         RunApp.start(AutoObjectApp.class);
+    }
+
+    private <T> T dispatch(int id, $.Function<Foo, T> extractor) {
+        switch (id) {
+            case 1:
+                return extractor.apply(foo1);
+            case 2:
+                return extractor.apply(foo2);
+            default:
+                System.out.println("unknown id: " + id);
+                throw badRequest();
+        }
     }
 
 }
