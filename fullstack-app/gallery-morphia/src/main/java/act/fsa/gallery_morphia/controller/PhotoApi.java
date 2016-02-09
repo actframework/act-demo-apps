25e32937c550584e569ecf533b84a040297a1dc6
Gelin Luo <greenlaw110@gmail.com>
2016-02-10 00:24:44 +1100

    update gallery-morphia

--- fullstack-app/gallery-morphia/src/main/java/act/fsa/gallery_morphia/controller/PhotoApi.java
+++ fullstack-app/gallery-morphia/src/main/java/act/fsa/gallery_morphia/controller/PhotoApi.java
@@ -6,9 +6,11 @@ import act.util.PropertySpec;
 import org.osgl.$;
 import org.osgl.mvc.annotation.GetAction;
 import org.osgl.mvc.annotation.PostAction;
+import org.osgl.mvc.result.Result;
 import org.osgl.storage.impl.SObject;
 
 import javax.inject.Inject;
+import javax.naming.ldap.Control;
 import java.io.File;
 
 /**
@@ -31,12 +33,13 @@ public class PhotoApi {
     }
 
     @PostAction
-    public void add(String title, String description, SObject image) {
+    public Result add(String title, String description, SObject image) {
         Photo photo = new Photo();
         photo.setTitle(title);
         photo.setDescription(description);
         photo.setImage(image);
         photoDao.save(photo);
+        return Controller.Util.redirect("/");
     }
 
 }
--- fullstack-app/gallery-morphia/src/main/java/act/fsa/gallery_morphia/model/Photo.java
+++ fullstack-app/gallery-morphia/src/main/java/act/fsa/gallery_morphia/model/Photo.java
@@ -3,23 +3,35 @@ package act.fsa.gallery_morphia.model;
 import act.data.Data;
 import act.db.morphia.MorphiaDao;
 import act.db.morphia.MorphiaModel;
-import act.storage.Managed;
 import act.storage.StorageServiceManager;
+import act.storage.Store;
 import org.bson.types.ObjectId;
+import org.imgscalr.Scalr;
 import org.mongodb.morphia.annotations.Entity;
 import org.mongodb.morphia.annotations.Property;
-import org.mongodb.morphia.annotations.Transient;
 import org.osgl.storage.ISObject;
 import org.osgl.storage.IStorageService;
+import org.osgl.storage.impl.SObject;
+import org.osgl.util.E;
 
-@Entity("img")
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+@Entity("photo")
 @Data
 public class Photo extends MorphiaModel {
 
-    public static final String VIEW = "title,description,imageUrl";
+    public static final String VIEW = "title,description,imageUrl,thumbnailUrl";
+    public static final int THUMBNAIL_WIDTH = 200;
 
-    @Managed
+    @Store("img")
     private ISObject image;
+
+    @Store("thumbnail")
+    private ISObject thumbnail;
 
     private String title;
 
@@ -32,6 +44,7 @@ public class Photo extends MorphiaModel {
 
     public void setImage(ISObject image) {
         this.image = image;
+        genThumbnail();
     }
 
     public String getTitle() {
@@ -46,6 +59,10 @@ public class Photo extends MorphiaModel {
         return null == image ? "" : image.getAttribute(ISObject.ATTR_URL);
     }
 
+    public String getThumbnailUrl() {
+        return null == thumbnail ? "" : thumbnail.getAttribute(ISObject.ATTR_URL);
+    }
+
     public String getDescription() {
         return description;
     }
@@ -54,10 +71,52 @@ public class Photo extends MorphiaModel {
         this.description = description;
     }
 
+    private void genThumbnail() {
+        if (null == image) {
+            return;
+        }
+        try {
+            BufferedImage img = ImageIO.read(image.asInputStream());
+            BufferedImage imgThumb = Scalr.resize(img, Scalr.Mode.AUTOMATIC, THUMBNAIL_WIDTH);
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            saveAwtImage(imgThumb, baos);
+            thumbnail = SObject.of(baos.toByteArray());
+            thumbnail.setAttributes(image.getAttributes());
+        } catch (IOException e) {
+            throw E.ioException(e);
+        }
+    }
+
+    private static void saveAwtImage(BufferedImage image, OutputStream os) {
+        int w = image.getWidth();
+        int h = image.getHeight();
+        BufferedImage after = new BufferedImage(w, h, BufferedImage.TYPE_3BYTE_BGR);
+        for (int y = 0; y < h; y++)
+            for (int x = 0; x < w; x++)
+                after.setRGB(x, y, image.getRGB(x, y));
+        try {
+            ImageIO.write(after, "jpg", os);
+        } catch (IOException e) {
+            throw E.ioException(e);
+        }
+    }
+
     // The DAO class corresponding to the Image entity
     public static class Dao extends MorphiaDao<ObjectId, Photo, Dao> {
         public Dao() {
             super(Photo.class);
+        }
+
+        public void delete(Photo photo) {
+            super.delete(photo);
+            deleteSObject(photo.image);
+            deleteSObject(photo.thumbnail);
+        }
+
+        private void deleteSObject(ISObject sobj) {
+            if (null != sobj) {
+                StorageServiceManager.instance().delete(sobj);
+            }
         }
     }
 
