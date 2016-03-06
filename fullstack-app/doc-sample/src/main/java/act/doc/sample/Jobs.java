f5dc705e12ecea9a0a0678669ce72a4fd18ff5fd
Gelin Luo <greenlaw110@gmail.com>
2016-03-07 07:59:05 +1100

    update doc-sample

--- fullstack-app/doc-sample/src/main/java/act/doc/sample/Jobs.java
+++ fullstack-app/doc-sample/src/main/java/act/doc/sample/Jobs.java
@@ -2,6 +2,7 @@ package act.doc.sample;
 
 import act.job.AlongWith;
 import act.job.Every;
+import act.job.OnAppStart;
 
 public class Jobs {
     @Every("50s")
@@ -12,6 +13,11 @@ public class Jobs {
     @AlongWith("act.doc.sample.Jobs.a")
     public void b() {
         System.out.println("job_b");
+    }
+
+    @OnAppStart
+    public void c() {
+
     }
 
 }
