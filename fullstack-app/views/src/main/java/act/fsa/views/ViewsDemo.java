d9a55eadfd40209468d1cb779cd3f0160bb09f15
Gelin Luo <greenlaw110@gmail.com>
2016-09-19 12:16:37 +1000

    add error demo cases

--- fullstack-app/views/src/main/java/act/fsa/views/ViewsDemo.java
+++ fullstack-app/views/src/main/java/act/fsa/views/ViewsDemo.java
@@ -1,5 +1,6 @@
 package act.fsa.views;
 
+import act.Act;
 import act.boot.app.RunApp;
 import act.controller.Controller;
 import act.inject.param.NoBind;
@@ -13,9 +14,19 @@ public class ViewsDemo extends Controller.Util {
     private String title = "ActFramework View Demo";
     private String who = "ActFramework";
 
+    @GetAction("e500")
+    public String backendServerError() {
+        // this will trigger a runtime error in the backend
+        return Act.crypto().decrypt("bad-crypted-msg");
+    }
+
     @GetAction({"", "rythm"})
-    public void home() {
+    public void rythm() {
         render(title, who);
+    }
+
+    @GetAction("rythm/error")
+    public void rythmTemplateError() {
     }
 
     @GetAction("beetl")
@@ -23,14 +34,26 @@ public class ViewsDemo extends Controller.Util {
         render(title, who);
     }
 
+    @GetAction("beetl/error")
+    public void beetlTemplateError() {
+    }
+
     @GetAction("velocity")
     public void velocity() {
         throw renderTemplate(title, who);
     }
 
+    @GetAction("velocity/error")
+    public void velocityTemplateError() {
+    }
+
     @GetAction("freemarker")
     public Result freemarker() {
         return renderTemplate(title, who);
+    }
+
+    @GetAction("freemarker/error")
+    public void freemarkerTemplateError() {
     }
 
     @GetAction("/api/v1/greeting/{who}")
