9536255e5ecccb0a709366ed820152189ccf8694
Gelin Luo <greenlaw110@gmail.com>
2016-10-12 14:24:57 +1100

    new simple helloworld demo project

---
+++ helloworld/src/main/java/act/helloworld/HelloWorldApp.java
@@ -0,0 +1,26 @@
+package act.helloworld;
+
+import act.boot.app.RunApp;
+import org.osgl.mvc.annotation.GetAction;
+
+import static act.controller.Controller.Util.render;
+
+/**
+ * The simple hello world app.
+ * <p>Run this app, try to update some of the code, then
+ * press F5 in the browser to watch the immediate change
+ * in the browser!</p>
+ */
+@SuppressWarnings("unused")
+public class HelloWorldApp {
+
+    @GetAction
+    public void home(String who) {
+        render(who);
+    }
+
+    public static void main(String[] args) throws Exception {
+        RunApp.start(HelloWorldApp.class);
+    }
+
+}
