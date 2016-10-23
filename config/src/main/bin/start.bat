4a2479593a12bf15cbd86e5b79fa36310f5b94a9
Gelin Luo <greenlaw110@gmail.com>
2016-10-23 15:25:22 +1100

    WIP

---
+++ config/src/main/bin/start.bat
@@ -0,0 +1,6 @@
+@echo off
+setlocal & pushd
+set BASE=%~dp0
+set CP=%BASE%\classes;%BASE%\lib\*
+java -Dapp.mode=prod -Dprofile=%ACT_PROFILE% -cp "%CP%" demo.injection.InjectionApp
+endlocal & popd
--- injection/src/main/java/demo/injection/InjectionApp.java
+++ injection/src/main/java/demo/injection/InjectionApp.java
@@ -1,20 +1,30 @@
 package demo.injection;
 
+import act.Version;
 import act.app.ActionContext;
 import act.boot.app.RunApp;
+import act.security.CSRF;
 import org.osgl.http.H;
 import org.osgl.inject.annotation.Provided;
 import org.osgl.mvc.annotation.Before;
 import org.osgl.mvc.annotation.GetAction;
+import org.osgl.mvc.annotation.PostAction;
+import org.osgl.mvc.annotation.PutAction;
+import org.osgl.mvc.result.Result;
+import org.osgl.util.C;
 
 import javax.inject.Inject;
 
+import java.util.Map;
+
+import static act.controller.Controller.Util.jsonMap;
+import static act.controller.Controller.Util.render;
 import static act.controller.Controller.Util.text;
 
 public class InjectionApp {
 
-    HiService hi;
-    ByeService bye;
+    private HiService hi;
+    private ByeService bye;
 
     @Inject
     public InjectionApp(HiService hi, ByeService bye) {
@@ -32,7 +42,7 @@ public class InjectionApp {
 
 
     @GetAction("/greeting")
-    public String greeting(@Provided GreetingService greeting) {
+    public String greeting(GreetingService greeting) {
         return greeting.greeting();
     }
 
@@ -40,19 +50,26 @@ public class InjectionApp {
     public void home() {
     }
 
-    @GetAction("/hi")
-    public String hi(String who) {
-        return hi.sayHi(who);
+    @PostAction("/hi")
+    public Result hi(String who) {
+        String message = hi.sayHi(who);
+        return render(message);
     }
 
-    @GetAction("/bye")
+    @PostAction("/bye")
     public void bye(String who) {
         text(bye.bye(who));
     }
 
+    @PutAction("/customer/{id}")
+    @CSRF.Disable
+    public Result updateCustomer(int id, String name, String age) {
+        return jsonMap(id, name, age);
+    }
+
 
     public static void main(String[] args) throws Exception {
-        RunApp.start(InjectionApp.class);
+        RunApp.start("Inject Demo", Version.appVersion(), InjectionApp.class);
     }
 
 }
