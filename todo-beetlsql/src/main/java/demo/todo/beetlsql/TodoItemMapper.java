b17c9e1f6eac3b131e91eacd8e77be9203291fb6
xiandafu <xiandafu@126.com>
2017-04-05 17:52:08 +0800

    refine demo

--- todo-beetlsql/src/main/java/demo/todo/beetlsql/TodoItemMapper.java
+++ todo-beetlsql/src/main/java/demo/todo/beetlsql/TodoItemMapper.java
@@ -1,6 +1,9 @@
 package demo.todo.beetlsql;
 
+import java.util.List;
+
 import org.beetl.sql.core.mapper.BaseMapper;
 
 public interface TodoItemMapper extends BaseMapper<TodoItem> {
+	public List<TodoItem> selectAll();
 }
