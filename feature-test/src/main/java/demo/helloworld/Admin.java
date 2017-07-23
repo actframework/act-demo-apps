23872b20d24ab6b5206157891404a047944c070a
Gelin Luo <greenlaw110@gmail.com>
2017-07-24 09:41:37 +1000

    add progress sample project

--- feature-test/src/main/java/demo/helloworld/Admin.java
+++ feature-test/src/main/java/demo/helloworld/Admin.java
@@ -1,10 +1,16 @@
 package demo.helloworld;
 
+import act.cli.Command;
+import act.cli.ReportProgress;
+import act.cli.Required;
 import act.controller.annotation.UrlContext;
+import act.util.Async;
+import act.util.ProgressGauge;
 import org.osgl.mvc.annotation.GetAction;
 import org.osgl.util.S;
 
 @UrlContext("admin")
+@SuppressWarnings("unused")
 public class Admin {
 
     @GetAction("random")
@@ -12,4 +18,30 @@ public class Admin {
         return S.random();
     }
 
+    @Async
+    @Command("countdown.async")
+    public void testAsncCommand(
+            @Required("specify the number") int num,
+            ProgressGauge progressGauge
+    ) throws Exception {
+        progressGauge.updateMaxHint(num);
+        for (int i = 0; i < num; ++i) {
+            progressGauge.step();
+            Thread.sleep(500);
+        }
+    }
+
+    @Async
+    @ReportProgress(type = ReportProgress.Type.TEXT)
+    @Command("countdown")
+    public void countDown(
+            @Required("specify the number") int num,
+            ProgressGauge progressGauge
+    ) throws Exception {
+        progressGauge.updateMaxHint(num);
+        for (int i = 0; i < num; ++i) {
+            progressGauge.step();
+            Thread.sleep(500);
+        }
+    }
 }
---
+++ progress/src/main/java/demo/progress/Application.java
@@ -0,0 +1,48 @@
+package demo.progress;
+
+import act.Act;
+import act.cli.Command;
+import act.cli.ReportProgress;
+import act.cli.Required;
+import act.util.Async;
+import act.util.ProgressGauge;
+import org.osgl.mvc.annotation.GetAction;
+
+@SuppressWarnings("unused")
+public class Application {
+
+    @GetAction
+    public void home() {
+    }
+
+    @Async
+    @Command(name = "countdown", help = "countdown the number in background")
+    public void countdown(
+            @Required("specify the number") int number,
+            ProgressGauge progressGauge
+    ) throws Exception {
+        progressGauge.updateMaxHint(number);
+        for (int i = 0; i < number; ++i) {
+            progressGauge.step();
+            Thread.sleep(500);
+        }
+    }
+
+    @Async
+    @ReportProgress
+    @Command(name = "countdown.track", help = "countdown the number and track")
+    public void countdownAndReportProgress(
+            @Required("specify the number") int number,
+            ProgressGauge progressGauge
+    ) throws Exception {
+        progressGauge.updateMaxHint(number);
+        for (int i = 0; i < number; ++i) {
+            progressGauge.step();
+            Thread.sleep(200);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Act.start("progress");
+    }
+}
