a94714afdc7bb99d4df9370875eb03065fc39e09
Gelin Luo <greenlaw110@gmail.com>
2017-08-30 17:52:48 +1000

    revamp helloworld sample pom file

--- feature-test/src/main/java/demo/helloworld/Admin.java
+++ feature-test/src/main/java/demo/helloworld/Admin.java
@@ -1,7 +1,11 @@
 package demo.helloworld;
 
 import act.cli.Command;
+import act.cli.ReportProgress;
+import act.cli.Required;
 import act.controller.annotation.UrlContext;
+import act.util.Async;
+import act.util.ProgressGauge;
 import org.osgl.mvc.annotation.GetAction;
 import org.osgl.util.S;
 
@@ -14,6 +18,32 @@ public class Admin {
         return S.random();
     }
 
+    @Async
+    @Command("countdown.async")
+    public void testAsncCommand(
+            @Required("specify the number") int num,
+            ProgressGauge progressGauge
+    ) throws Exception {
+        progressGauge.updateMaxHint(num);
+        for (int i = 0; i < num; ++i) {
+            progressGauge.step();
+            Thread.sleep(500);
+        }
+    }
+
+    @Async
+    @ReportProgress(type = ReportProgress.Type.TEXT)
+    @Command("countdown")
+    public void countDown(
+            @Required("specify the number") int num,
+            ProgressGauge progressGauge
+    ) throws Exception {
+        progressGauge.updateMaxHint(num);
+        for (int i = 0; i < num; ++i) {
+            progressGauge.step();
+            Thread.sleep(500);
+        }
+    }
 
     @Command("gh322")
     public static void cliHandlerWithException() {
